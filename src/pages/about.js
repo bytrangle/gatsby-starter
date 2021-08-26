72067992833fb094113a0131459674f18959b818
Trang Le <lethutrang101@gmail.com>
2021-08-26 15:28:48 +0700

    Add About page

---
+++ src/pages/about.js
@@ -0,0 +1,10 @@
+import * as React from "react"
+import Layout from "../components/layout"
+
+export default function About() {
+  return (
+    <Layout pageTitle="About Me">
+      <p>Just a person who can't resist the urge to build and break websites.</p>
+    </Layout>
+  )
+}
